aad67e307e1ebfe7ff596b1adfa73a1c59a6f755
sksamuel <sam@sksamuel.com>
2019-02-04 14:52:42 +0000

    Aded iso time and iso date

---
+++ src/main/java/io/lenses/sql/udf/isoformattime.java
@@ -0,0 +1,20 @@
+package io.lenses.sql.udf;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Formats a timestamp representing milliseconds since the unix epoch as an ISO Date Time formatted string.
+ */
+public class isoformattime implements UserDefinedFunction1 {
+
+    @Override
+    public Object evaluate(Object o) {
+        if (o == null) return null;
+        long timestamp = Long.parseLong(o.toString());
+        OffsetDateTime dt = Instant.ofEpochMilli(timestamp).atOffset(ZoneOffset.UTC);
+        return DateTimeFormatter.ISO_TIME.format(dt);
+    }
+}
