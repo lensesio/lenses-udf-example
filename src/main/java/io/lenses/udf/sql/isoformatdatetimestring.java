94737890dbc88a894c54e5bcf05e81b51a1d928d
sksamuel <sam@sksamuel.com>
2019-02-04 14:18:10 +0000

    Changed iso function to use UserDefinedFunction1

--- src/main/java/io/lenses/udf/sql/isoformatdatetimestring.java
+++ src/main/java/io/lenses/udf/sql/isoformatdatetimestring.java
@@ -1,20 +1,21 @@
 package io.lenses.udf.sql;
 
-import io.lenses.sql.udf.UserDefinedFunction;
+import io.lenses.sql.udf.UserDefinedFunction1;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 /**
  * Formats a timestamp representing milliseconds since the unix epoch as an ISO Date Time formatted string.
  */
-public class isoformatdatetimestring implements UserDefinedFunction {
+public class isoformatdatetimestring implements UserDefinedFunction1 {
+
     @Override
-    public Object evaluate(List<Object> list) {
-        long timestamp = Long.parseLong(list.get(0).toString());
+    public Object evaluate(Object o) {
+        if (o == null) return null;
+        long timestamp = Long.parseLong(o.toString());
         OffsetDateTime dt = Instant.ofEpochMilli(timestamp).atOffset(ZoneOffset.UTC);
         return DateTimeFormatter.ISO_DATE_TIME.format(dt);
     }
